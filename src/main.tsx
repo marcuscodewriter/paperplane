e4a095d8c3150d2bd1ce53f78df3523a040c760c
M3 Beatz <m3beatz@M3.local>
2024-03-27 16:33:14 -0400

    Styling loading screen

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,9 @@ import ReactDOM from 'react-dom/client';
 import { Game } from './components/Game';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
+    <div id="gameContainer" style={{ width: '100vw', height: '100vh' }}>
     <Game />
-  // </React.StrictMode>,
+    </div>
+  </React.StrictMode>,
 );
