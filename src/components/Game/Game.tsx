c30d48e615b04ac86dd8d5addbe7bbe89ca51a76
M3 Beatz <m3beatz@M3.fios-router.home>
2024-03-27 03:11:54 -0400

    Initial

---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,57 @@
+import { FC, Fragment, useEffect } from "react";
+import { Unity, useUnityContext } from 'react-unity-webgl';
+import { useInitData } from '@vkruglikov/react-telegram-web-app';
+import './Game.css';
+
+export const Game: FC = () => {
+  const { unityProvider, sendMessage, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: "Build/PaperPlaneGame.loader.js",
+    dataUrl: "Build/PaperPlaneGame.data",
+    frameworkUrl: "Build/PaperPlaneGame.framework.js",
+    codeUrl: "Build/PaperPlaneGame.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "$PLANE",
+    productName: "Paper $PLANE Game",
+    productVersion: "0.4",
+  });
+
+  const [initDataUnsafe] = useInitData();
+
+  const importUsername = () => {
+    var user = initDataUnsafe?.user;
+    var username = "Guest"
+    if (user) {
+      if (user.username && user.username.length > 0) {
+        username = user.username;
+      } else if (user.first_name && user.first_name.length > 0) {
+        username = user.first_name;
+      }
+    }
+    sendMessage('Menu Manager', 'ReceiveTelegramUsername', username);
+    console.log(username);
+  }
+
+  useEffect(() => {
+    if (isLoaded) {
+      importUsername();
+    }
+  }, [isLoaded]);
+
+  return (
+    <>
+      <div id="gameContainer" style={{ width: '100vw', height: '100vh' }}>
+        <Fragment>
+        {!isLoaded && (
+          <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+        )}
+          <Unity
+          devicePixelRatio={2}
+            id="unity-canvas"
+            unityProvider={unityProvider}
+            style={{ width: '100%', height: '100%', visibility: isLoaded ? 'visible' : 'hidden'}}
+          />
+        </Fragment>
+      </div>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Root } from './components/Root';
+import { Game } from './components/Game';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Root />
+    <Game />
   </React.StrictMode>,
 );
